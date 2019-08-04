1043cbd7025f58ad7348106014b64e22909b46cc
Sakuraba Kiyoka <SakurabaKiyoka@Gmail.com>
2019-08-05 00:40:35 +0900

    Auto config grub2

--- inst-pre.sh
+++ inst-pre.sh
@@ -3,5 +3,8 @@ rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 yum install yum-plugin-fastestmirror -y
 yum --enablerepo=elrepo-kernel install kernel-ml -y
-grub2-set-default 0
+currker=$(awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg |grep '(5.')
+grub2-set-default "$currker"
+grub2-editenv list
+grub2-mkconfig -o /boot/grub2/grub.cfg
 reboot

91c1355de4ac61416a9b5c45dcf764f4a24b2b01
Sakuraba Kiyoka <SakurabaKiyoka@Gmail.com>
2020-10-27 22:36:13 +0900

    Update inst-pre.sh

--- inst-pre.sh
+++ inst-pre.sh
@@ -1,6 +1,7 @@
 #/usr/bin/bash
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+yum install elrepo-release
 yum install yum-plugin-fastestmirror -y
 yum --enablerepo=elrepo-kernel install kernel-ml -y
 currker=$(awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg |grep '(5.')
